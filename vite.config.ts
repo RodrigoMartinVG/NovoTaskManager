ecf88d27056e2b69cb03fb09d9efb6403cdb54e9
Rodrigo Martin <rodrigomartin.vg@gmail.com>
2026-03-31 12:15:33 -0300

    folder root for production: NovoTaskManager

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/NovoTaskManager/' : '/',
   build: {
     target: 'es2022',
     outDir: 'dist',
